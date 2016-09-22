e5782e82f2e5fabf1d0b829991b162cdde2cd9f0
amemon7 <afzal.memon@yahoo.com>
2016-09-21 22:45:13 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+require(MASS)
+makeCacheMatrix <- function(x = matrix()) { 
+  minv <- NULL                     
+  set <- function(y) {                      
+    x <<- y
+    minv <<- NULL              
+  }
+  get <- function() x
+  #using ginv function from MASS - Solve can also be used instead
+  setinv <- function(ginv) minv <<- ginv 
+  getinv <- function() minv        
+  list(set = set, get = get,                    
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<- function(x, ...) {                 
+  minv <- x$getinv()
+  if(!is.null(minv)) {                 
+    message("amemon7:Getting Cached Data")
+    return(minv)
+  }
+  data <- x$get()                               
+  minv <- ginv(data, ...)
+  x$setinv(minv)
+  minv
 }
